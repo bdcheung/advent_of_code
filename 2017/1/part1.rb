bca98ed6c86b8c92ddd2427b1f391e1ce4c8b6f3
Brian Cheung <brian.cheung@phishme.com>
2017-12-01 12:20:50 -0500

    Submitting part 1 of 2

---
+++ 2017/1/part1.rb
@@ -0,0 +1,32 @@
+require 'pry'
+class Foo
+  attr_accessor :sum, :array, :raw
+  def initialize(number)
+    @raw = number
+    @array = []
+    @sum = 0
+
+    split_to_array
+  end
+
+  def result
+    array.each_with_index do |num, idx|
+      self.sum = self.sum + num.to_i if matched_pairs(num, idx)
+    end
+    sum
+  end
+
+  private
+
+  def split_to_array
+    raw.to_s.split('').each {|x| array << x}
+  end
+
+  def matched_pairs(num, idx)
+    num == partner_for(num, idx)
+  end
+
+  def partner_for(num, idx)
+    array.rotate(idx + 1).first
+  end
+end
